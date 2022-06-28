bb266adf8fcd92d00afea189526a298f373a9044
JohnParkerLee <johnparker@163.com>
2022-06-28 22:37:20 +0800

    4 Solid Texture

---
+++ 2 Motion Blur/hittable_list.h
@@ -0,0 +1,57 @@
+#ifndef HITTABLE_LIST_H
+#define HITTABLE_LIST_H
+
+#include "hittable.h"
+
+#include <memory>
+#include <vector>
+
+using std::make_shared;
+using std::shared_ptr;
+
+class hittable_list : public hittable
+{
+public:
+    hittable_list() {}
+    hittable_list(shared_ptr<hittable> object) { add(object); }
+
+    void clear()
+    {
+        objects.clear();
+    }
+
+    void add(shared_ptr<hittable> object)
+    {
+        objects.push_back(object);
+    }
+
+    virtual bool hit(const ray &r, double t_min, double t_max, hit_record &rec) const override;
+
+public:
+    /*
+        shared_ptr<double> double_ptr = make_shared<double>(0.37);
+        shared_ptr<vec3>   vec3_ptr   = make_shared<vec3>(1.414214, 2.718281, 1.618034);
+        shared_ptr<sphere> sphere_ptr = make_shared<sphere>(point3(0,0,0), 1.0);
+    */
+    std::vector<shared_ptr<hittable>> objects;
+
+};
+
+bool hittable_list::hit(const ray &r, double t_min, double t_max, hit_record &rec) const
+{
+    hit_record temp_rec;
+    bool hit_anything = false;
+    auto closet_so_far = t_max;
+
+    for(const auto& object:objects){
+        if(object->hit(r, t_min, closet_so_far, temp_rec)){
+            hit_anything = true;
+            closet_so_far = temp_rec.t;
+            rec = temp_rec;
+        }
+    }
+
+    return hit_anything;
+}
+
+#endif
---
+++ 3 AABB/sphere.h
@@ -0,0 +1,64 @@
+#ifndef SPHERE_H
+#define SPHERE_H
+
+#include "hittable.h"
+#include "vec3.h"
+#include "aabb.h"
+
+class sphere : public hittable
+{
+public:
+    sphere() {}
+    sphere(point3 cen, double r, shared_ptr<material> m) : center(cen), radius(r), mat_ptr(m) {}
+
+    virtual bool hit(const ray &r, double t_min, double t_max, hit_record &rec) const override;
+    virtual bool bounding_box(double time0, double time1, aabb& output_box) const override;
+
+public:
+    point3 center;
+    double radius;
+    shared_ptr<material> mat_ptr;
+};
+
+bool sphere::hit(const ray &r, double t_min, double t_max, hit_record &rec) const
+{
+    vec3 oc = r.origin() - center;
+    auto a = r.direction().length_squared();
+    auto half_b = dot(oc, r.direction());
+    auto c = oc.length_squared() - radius * radius;
+
+    auto discriminant = half_b * half_b - a * c;
+    if (discriminant < 0)
+    {
+        return false;
+    }
+    auto sqrtd = sqrt(discriminant);
+
+    // Find the nearest root that lies in the acceptable range.
+    // half_b<0 when the angle > 180
+    auto root = (-half_b - sqrtd) / a;
+    if (root < t_min || t_max < root)
+    {
+        root = (-half_b + sqrtd) / a;
+        if (root < t_min || t_max < root)
+            return false;
+    }
+
+    rec.t = root;
+    rec.p = r.at(rec.t);
+    vec3 outward_normal = (rec.p - center) / radius;
+    rec.set_face_normal(r, outward_normal);
+    rec.mat_ptr = mat_ptr;
+
+    return true;
+}
+
+bool sphere::bounding_box(double time0, double time1, aabb &output_box) const
+{
+    output_box = aabb(
+        center - vec3(radius, radius, radius),
+        center + vec3(radius, radius, radius));
+    return true;
+}
+
+#endif
