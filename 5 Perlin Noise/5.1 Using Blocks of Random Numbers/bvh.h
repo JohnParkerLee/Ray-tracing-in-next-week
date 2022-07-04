917ab21e471d71fb2b8075f64a6e1c9e8019f7e4
JohnParkerLee <johnparker@163.com>
2022-07-04 21:31:38 +0800

    fix bvh tree bug and add 9 Volumes

--- 5 Perlin Noise/5.1 Using Blocks of Random Numbers/bvh.h
+++ 5 Perlin Noise/5.1 Using Blocks of Random Numbers/bvh.h
@@ -19,6 +19,29 @@ public:
 
     virtual bool bounding_box(double time0, double time1, aabb &output_box) const override;
 
+    inline bool box_compare(const shared_ptr<hittable> a, const shared_ptr<hittable> b, int axis)
+    {
+        aabb box_a, box_b;
+        if (!a->bounding_box(0, 0, box_a) || !b->bounding_box(0, 0, box_b))
+            std::cerr << "No bounding box in bvh_node constructor.\n";
+
+        return box_a.min().e[axis] < box_b.min().e[axis];
+    }
+    bool box_x_compare(const shared_ptr<hittable> a, const shared_ptr<hittable> b)
+    {
+        return box_compare(a, b, 0);
+    }
+
+    bool box_y_compare(const shared_ptr<hittable> a, const shared_ptr<hittable> b)
+    {
+        return box_compare(a, b, 1);
+    }
+
+    bool box_z_compare(const shared_ptr<hittable> a, const shared_ptr<hittable> b)
+    {
+        return box_compare(a, b, 2);
+    }
+
 public:
     shared_ptr<hittable> left;
     shared_ptr<hittable> right;
@@ -81,29 +104,6 @@ bool bvh_node::hit(const ray &r, double t_min, double t_max, hit_record &rec) co
     bool hit_right = right->hit(r, t_min, hit_left ? rec.t : t_max, rec);
 
     return hit_left || hit_right;
-}
-
-inline bool box_compare(const shared_ptr<hittable> a, const shared_ptr<hittable> b, int axis)
-{
-    aabb box_a, box_b;
-    if (!a->bounding_box(0, 0, box_a) || !b->bounding_box(0, 0, box_b))
-        std::cerr << "No bounding box in bvh_node constructor.\n";
-
-    return box_a.min().e[axis] < box_b.min().e[axis];
-}
-bool box_x_compare(const shared_ptr<hittable> a, const shared_ptr<hittable> b)
-{
-    return box_compare(a, b, 0);
-}
-
-bool box_y_compare(const shared_ptr<hittable> a, const shared_ptr<hittable> b)
-{
-    return box_compare(a, b, 1);
-}
-
-bool box_z_compare(const shared_ptr<hittable> a, const shared_ptr<hittable> b)
-{
-    return box_compare(a, b, 2);
 }
 
 #endif
